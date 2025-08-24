ffdcd66d68a88b3a22a4c3ded27bd70e49363474
AidaG91 <aida.garmus@gmail.com>
2025-08-24 15:13:26 +0200

    Test create customer / create flight / find customer by name

---
+++ src/test/java/FlyingApp/demo/controller/CustomerControllerTest.java
@@ -0,0 +1,67 @@
+package FlyingApp.demo.controller;
+
+import FlyingApp.demo.enums.CustomerStatus;
+import FlyingApp.demo.model.Customer;
+import FlyingApp.demo.service.CustomerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CustomerController.class)
+
+public class CustomerControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private CustomerService customerService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Customer juan, helena;
+
+    @BeforeEach
+    public void setUp(){
+        juan = new Customer("Juan", CustomerStatus.SILVER, 1200);
+        helena = new Customer("Helena", CustomerStatus.NONE, 350);
+    }
+
+    @Test
+    public void testCreateCustomer() throws Exception{
+        when(customerService.createCustomer(juan)).thenReturn(juan);
+
+        mockMvc.perform(post("/customers")
+                .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(juan)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.customerName").value("Juan"))
+                .andExpect(jsonPath("$.customerStatus").value("SILVER"))
+                .andExpect(jsonPath("$.totalCustomerMileage").value(1200));
+    }
+
+    @Test
+    public void testGetCustomerByName() throws Exception{
+        String customerName = "Helena";
+
+        when(customerService.findByCustomerName(customerName)).thenReturn(List.of(helena));
+
+        mockMvc.perform(get("/customers/find/{customerName}", customerName).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].customerName").value("Helena"));
+
+    }
+
+}
---
+++ src/test/java/FlyingApp/demo/controller/FlightControllerTest.java
@@ -0,0 +1,50 @@
+package FlyingApp.demo.controller;
+
+import FlyingApp.demo.model.Flight;
+import FlyingApp.demo.service.FlightService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FlightController.class)
+
+public class FlightControllerTest {
+   @Autowired
+   private MockMvc mockMvc;
+
+   @MockitoBean
+    private FlightService flightService;
+
+   @Autowired
+    private ObjectMapper objectMapper;
+
+   private Flight boeing, airbus;
+
+   @BeforeEach
+    public void setUp() {
+       boeing = new Flight("DL143", "Boeing 747", 400, 135);
+       airbus = new Flight("DL122", "Airbus A330", 236, 4370);
+   }
+
+   @Test
+    public void testCreateFlight() throws Exception{
+       when(flightService.createFlight(boeing)).thenReturn(boeing);
+
+       mockMvc.perform(post("/flights").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(boeing)))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.flightNumber").value("DL143"))
+               .andExpect(jsonPath("$.aircraft").value("Boeing 747"))
+               .andExpect(jsonPath("$.totalAircraftSeats").value(400))
+               .andExpect(jsonPath("$.flightMileage").value(135));
+   }
+}
